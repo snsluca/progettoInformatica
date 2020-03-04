7a8f53695ae437146ac19e0e74319c291218c9ae
snsluca <snsluca@gmail.com>
2020-03-04 14:33:00 +0100

    pushSanson

--- pushIannotta.bat
+++ pushIannotta.bat
@@ -1,7 +1,7 @@
 git init
-git checkout -b dev
+git checkout -b devIannotta
 git add -A
 git remote add origin https://github.com/snsluca/progettoInformatica.git
-git commit -m "pushSanson"
-git push -u origin dev
+git commit -m "pushIannotta"
+git push -u origin devIannotta
 PAUSE
